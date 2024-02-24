9fa2f88a60b961fa28a35e2afa56e3ea1035ad05
Ben Püschel <ben.pueschel2004@gmail.com>
2024-02-24 02:38:41 +0100

    impl display for span

--- src/span.rs
+++ src/span.rs
@@ -1,7 +1,15 @@
+use std::fmt::Display;
+
 #[derive(Debug, Clone, Copy, PartialEq, Eq)]
 pub struct Span {
     pub start: usize,
     pub end: usize,
+}
+
+impl Display for Span {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        write!(f, "({}, {})", self.start, self.end)
+    }
 }
 
 impl From<(usize, usize)> for Span {
